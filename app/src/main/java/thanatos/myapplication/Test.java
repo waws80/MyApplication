3387628b339c9a78e3fce7020fbd59d00173e78a
thanatos <523928638@qq.com>
2016-09-02 23:35:33 +0800

    提交测试-添加内容

---
+++ app/src/main/java/thanatos/myapplication/Test.java
@@ -0,0 +1,12 @@
+package thanatos.myapplication;
+
+/**
+ * Created by lxf52 on 2016/9/2.
+ */
+
+public class Test {
+    private static void main(){
+        System.out.println("123");
+    }
+
+}
